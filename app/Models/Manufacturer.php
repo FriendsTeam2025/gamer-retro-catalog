e5ac6db9bc3a0dbbc6d5a8b19f50b1327b424743
Luan Martins <luan_18martins@hotmail.com>
2025-07-07 19:59:20 -0300

    feat: implement model

--- app/Models/Manufacturer.php
+++ app/Models/Manufacturer.php
@@ -3,9 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Manufacturer extends Model
 {
-    use HasUuids;
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'name',
+        'country_id',
+    ];
+
+
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(Country::class);
+    }
 }
